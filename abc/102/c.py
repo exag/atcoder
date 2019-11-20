6b85267c1a581595ed1dc8c9d0f68d4ef340f587
a a <none>
2019-11-20 23:58:16 +0900

    abc102-c

---
+++ abc/102/c.py
@@ -0,0 +1,19 @@
+# 方針
+# |(A1-1)-x| + |(A2-2)-x| + ... を最小とするxを求める
+# 数直線で考えた時、(A1-1)、(A2-2)、...への距離の合計が最小となるx
+# →xは中央値
+
+N = int(input())
+A = list(map(int, input().split()))
+# 中央値
+B = []
+for i in range(N):
+    B.append(A[i] - (i+1))
+b = sorted(B)[N//2]
+
+ans = 0
+for i in range(N):
+    ans += abs(A[i] - (i + 1) - b)
+
+print(ans)
+
