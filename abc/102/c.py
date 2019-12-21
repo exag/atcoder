3c5b742cfcaa24070fb7f703cca685e5357e9524
exag <hukuti145@gmail.com>
2019-12-21 16:41:41 +0900

    abc102-c 中央値はライブラリで計算する

--- abc/102/c.py
+++ abc/102/c.py
@@ -3,13 +3,16 @@
 # 数直線で考えた時、(A1-1)、(A2-2)、...への距離の合計が最小となるx
 # →xは中央値
 
+from statistics import median_low
+
+
 N = int(input())
 A = list(map(int, input().split()))
 # 中央値
 B = []
 for i in range(N):
     B.append(A[i] - (i+1))
-b = sorted(B)[N//2]
+b = median_low(B)
 
 ans = 0
 for i in range(N):
