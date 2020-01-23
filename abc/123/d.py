96a7d2325b79b6ee3c85d3a7381f3501ca4d138d
a a <none>
2020-01-23 21:34:43 +0900

    abc123-d

--- abc/123/d.py
+++ abc/123/d.py
@@ -1,7 +1,11 @@
-"""
-A*B*C(1000^3) は間に合わないけど、
-A*B(1000^2) を先にに計算して、その上位3000位*1000 なら間に合う
-※ pypy3 じゃないと間に合わない
+"""枝借り
+Aのランク*Bのランク*Cのランク <= K
+となるA, B, Cだけ考えればいい
+
+例えばK=8なら、それぞれ1〜2位を選ぶしかない
+3位以降を選ぶと絶対に8位以内には入れない
+
+これならpypyじゃなくても間に合う
 """
 X, Y, Z, K = map(int, input().split())
 
@@ -9,17 +13,15 @@ A = sorted(map(int, input().split()), reverse=True)
 B = sorted(map(int, input().split()), reverse=True)
 C = sorted(map(int, input().split()), reverse=True)
 
-AB = []
-for i in range(X):
-    for j in range(Y):
-        AB.append(A[i] + B[j])
-
-AB.sort(reverse=True)
-
 ABC = []
-for i in range(min(K, len(AB))):
-    for j in range(Z):
-        ABC.append(AB[i] + C[j])
+for a in range(X):
+    for b in range(Y):
+        if (a + 1) * (b + 1) > K:
+            break
+        for c in range(Z):
+            if (a + 1) * (b + 1) * (c + 1) > K:
+                break
+            ABC.append(A[a] + B[b] + C[c])
 
 ABC.sort(reverse=True)
 for i in range(K):
