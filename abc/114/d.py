a4dc5a3c05af3ab03de20307a8fb76646b5a8fb2
a a <none>
2020-01-25 15:27:10 +0900

    abc124-d

--- abc/114/d.py
+++ abc/114/d.py
@@ -1,17 +1,39 @@
 u"""
 解説PDF通り
 https://img.atcoder.jp/abc114/editorial.pdf
+
+一般に、ある数 N を
+N = P1^e1 * P2^e2 * ... * Pk^ek
+と表すとき、約数の数 D は
+D = (e1+1)*(e2+1)*...*(ek+1)
+となる
+
+
 """
+## 別解
+# N = int(input())
+#
+# # 1〜N をそれぞれ素因数分解して合算することで N! を素因数分解する
+# e = [0] * (N+1)
+# for i in range(2, N+1):
+#     cur = i
+#     for j in range(2, i+1):
+#         while cur % j == 0:
+#             e[j] += 1
+#             cur //= j
+
+from math import factorial
+
 N = int(input())
 
-# 1〜N をそれぞれ素因数分解して合算することで N! を素因数分解する
+# N! を素因数分解
+fact = factorial(N)
 e = [0] * (N+1)
 for i in range(2, N+1):
-    cur = i
-    for j in range(2, i+1):
-        while cur % j == 0:
-            e[j] += 1
-            cur //= j
+    while fact % i == 0:
+        e[i] += 1
+        fact //= i
+
 
 def num(m):
     return len(list(filter(lambda x: x >= m-1, e)))
