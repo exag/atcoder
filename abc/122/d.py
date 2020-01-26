080b71cee150f09cb032de69c202fbf52c650986
a a <none>
2020-01-26 16:19:12 +0900

    abc122-d

---
+++ abc/122/d.py
@@ -0,0 +1,33 @@
+"""DFS(メモ化再帰)
+
+"""
+from functools import lru_cache
+
+
+N = int(input())
+MOD = 10**9+7
+
+
+def ok(last4):
+    for i in range(4):
+        t = list(last4)
+        if i > 0:
+            t[i-1], t[i] = t[i], t[i-1]
+        if ''.join(t).count('AGC') > 0:
+            return False
+    return True
+
+
+@lru_cache(maxsize=1000)
+def dfs(cur, last3):
+    if cur == N:
+        return 1
+    ret = 0
+    for c in 'AGCT':
+        if ok(last3 + c):
+            ret += dfs(cur+1, last3[1:] + c)
+            ret %= MOD
+    return ret
+
+
+print(dfs(0, 'TTT'))
