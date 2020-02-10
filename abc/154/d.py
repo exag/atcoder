68ac111f4efe8c754ad6c9fc191e8e4f3e15fc47
exag <hukuti145@gmail.com>
2020-02-11 00:04:56 +0900

    abc154-d

--- abc/154/d.py
+++ abc/154/d.py
@@ -3,23 +3,13 @@ from itertools import accumulate
 
 N, K = map(int, input().split())
 P = list(map(int, input().split()))
+E = [(p+1)/2 for p in P]
 
-d = {}
-for i in range(1, 1001):
-    d[i] = (i + 1) * (i / 2) / i
+cum = [0]
+cum.extend(list(accumulate(E)))
 
-E = []
-for p in P:
-    E.append((p+1)/2)
-
-cum = list(accumulate(E))
-mx = 0
-idx = 0
 ans = 0
 for i in range(N-K+1):
-    if i == 0:
-        mx = max(mx, cum[i+K-1])
-    else:
-        mx = max(mx, cum[i+K-1] - cum[i-1])
+    ans = max(ans, cum[i+K] - cum[i])
 
-print(mx)
+print(ans)
