52555e9d15103394e601809412bb1ca1c0f45c35
a a <hukuti145@gmail.com>
2020-02-21 21:54:13 +0900

    abc067-d

---
+++ abc/067/d.py
@@ -0,0 +1,11 @@
+"""貪欲法
+"""
+N, A, B = map(int, input().split())
+X = list(map(int, input().split()))
+
+ans = 0
+for i in range(1, N):
+    d = X[i] - X[i-1]
+    ans += min(d * A, B)
+
+print(ans)
