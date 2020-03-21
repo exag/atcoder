893f3d9f421725ef2d85c1044816b4ea8764f62a
a a <hukuti145@gmail.com>
2020-03-21 19:39:36 +0900

    cf17-relay-open-a

---
+++ other/cf17-relay-open/a.py
@@ -0,0 +1,17 @@
+"""
+math.ceilは巨大な数字の場合は誤差が出るので、自分で用意する
+"""
+ceil = lambda a, b: -(-a // b)
+
+K, A, B = map(int, input().split())
+if A >= K:
+    print(1)
+    exit()
+p = A - B
+if p < 0:
+    print(-1)
+    exit()
+
+ans = 1
+ans += ceil(K - A, A - B) * 2
+print(ans)
