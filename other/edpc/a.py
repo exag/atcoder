ef286a90ec07599e524dcc50c597bd1e445f335d
a a <hukuti145@gmail.com>
2020-03-29 19:08:45 +0900

    edpc-a

--- other/edpc/a.py
+++ other/edpc/a.py
@@ -2,13 +2,12 @@ INF = float('inf')
 N = int(input())
 h = list(map(int, input().split()))
 
-dp = [INF for _ in range(N)]
+dp = [INF for i in range(N)]
 dp[0] = 0
-dp[1] = abs(h[1] - h[0])
+for i in range(N):
+    if i - 1 >= 0:
+        dp[i] = min(dp[i], dp[i - 1] + abs(h[i] - h[i - 1]))
+    if i - 2 >= 0:
+        dp[i] = min(dp[i], dp[i - 2] + abs(h[i] - h[i - 2]))
 
-for i in range(2, N):
-    cost1 = dp[i-1] + abs(h[i] - h[i-1])
-    cost2 = dp[i-2] + abs(h[i] - h[i-2])
-    dp[i] = min(cost1, cost2)
-
-print(dp[N-1])
+print(dp[N - 1])
